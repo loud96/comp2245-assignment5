e4ea4f667c01bc0bb66ef36e135fa2572e42c43e
Louetta Daly <500000504@my.fiveislands.uwi.edu>
2023-11-16 19:28:11 -0400

    Tables and styling added

--- world.php
+++ world.php
@@ -7,16 +7,32 @@ $dbname = 'world';
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 $country = isset($_GET['country']) ? $_GET['country'] : ''; // Get the country from the query parameter
 
-$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'"); //Retrieve countries with name 'like' the query country
+$stmt = $conn->query("SELECT name, continent, independence_year, head_of_state FROM countries WHERE name LIKE '%$country%'"); //Retrieve countries with name 'like' the query country
+// $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'"); //Retrieve countries with name 'like' the query country
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 
 
-
 ?>
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
+<table>
+  <thead>
+    <tr>
+      <th>Country</th>
+      <th>Continent</th>
+      <th>Independence Year</th>
+      <th>Head of State</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php foreach ($results as $row): ?>
+      <tr>
+        <td><?= $row['name']; ?></td>
+        <td><?= $row['continent']; ?></td>
+        <td><?= $row['independence_year']; ?></td>
+        <td><?= $row['head_of_state']; ?></td>
+      </tr>
+    <?php endforeach; ?>
+  </tbody>
+</table>
+
